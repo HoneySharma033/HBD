10558c54125c12139777ba45f045f61be2aca6db
HoneySharma033 <159022119+HoneySharma033@users.noreply.github.com>
2024-02-06 15:11:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,50 @@
+
+var config = {
+
+    texts: [
+        "To my special one,",     
+        "On your birthday today,",
+        "This marks our",
+        "First celebration together!",
+        "In these last 6 months, I've seen you",
+        "In various peculiar, cute, and",
+        "even angry moments...",
+        "All of them are my favorites.",
+        "BTW,Should we post a photo of us together on Instagram?",
+        "Hue Hue",
+        "May this year bring you endless happiness,",
+        "Eat to your heart's content,",
+        "Then you'll have the energy to",
+        "Work hard and think of me!",
+        "Happy Birthday Love",
+    ],
+    imgs: {
+        "To my special one,": "./imgs/songxinxin.jpeg",
+        "On your birthday today,": "./imgs/Birthday_Today.jpeg",
+        "This marks our": "./imgs/marks_our.jpeg",
+        "First celebration together!": "./imgs/First_Celeb.png",
+        "In these last 6 months, I've seen you": "./imgs/chuo.gif",
+        "In various peculiar, cute, and": "./imgs/peculiar_way.jpeg",
+        "even angry moments...": "./imgs/angry.jpeg",
+        "All of them are my favorites.": "./imgs/favorite_cute.png",
+        "BTW,Should we post a photo of us together on Instagram? ": "./imgs/Shou.png",
+        "Hue Hue": "./imgs/hehe.gif",
+        "May this year bring you endless happiness,":"./imgs/Hapiness.gif",
+        "Eat to your heart's content,": "./imgs/eat.jpeg",
+        "Then you'll have the energy to": "./imgs/Energy.jpeg",
+        "Work hard and think of me!": "./imgs/work_hard.jpeg",
+        "Happy Birthday Love": "./imgs/Happy_birthday.png"
+    },
+    desc: {
+        turn_on: "Click to Start",
+        play: "Chaliye Shuru karte hai",
+        bannar_coming: "Add Some Magic",
+        balloons_flying: "Seems Something's Missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday Sampoorna (My Rangabati)",
+        story: "A MESSAGE FOR YOU"
+    },
+    loveText: 'Yours and only yours, KARTIK SHARMA'
+};
+
